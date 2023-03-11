6713c234ec51b0af4a4a1b32a486d852effa9e6e
tuyana-stv <tuyana240696@gmail.com>
2023-03-11 20:18:28 +0300

    Добавляет миниатюры фотографий

--- js/util.js
+++ js/util.js
@@ -7,18 +7,32 @@ const getRandomInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const idList = [];
+const idCommentsList = [];
 
-const getRandomId = (element) => {
+const getRandomId = (element, list) => {
   let randomId = 0;
   do {
     randomId = getRandomInteger(1, element);
-  } while (idList.includes(randomId));
-  idList.push(randomId);
+  } while (list.includes(randomId));
+  list.push(randomId);
   return randomId;
+};
+
+const getRandomComment = (messages, names) => {
+  const comments = [];
+  for (let i = 0; i < getRandomInteger(1, 200); i++) {
+    comments.push({
+      idComment: getRandomId(6000, idCommentsList),
+      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+      message: getRandomArrayElement(messages),
+      name: getRandomArrayElement(names),
+    });
+  }
+  return comments;
 };
 
 export {getRandomInteger};
 export {getRandomArrayElement};
 export {getRandomId};
+export {getRandomComment};
 
